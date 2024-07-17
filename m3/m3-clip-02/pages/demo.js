16be944c6c12841a927854d2ae42dc1d0267515e
Cash <cashwugeek@icloud.com>
2024-07-17 17:25:13 +0800

    add demo.js

--- m3/m3-clip-02/pages/demo.js
+++ m3/m3-clip-02/pages/demo.js
@@ -1,22 +1,29 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react"
 
 export default function Demo() {
-  const [text1, setText1] = useState("First");
-  const [text2, setText2] = useState("Last");
 
-  return (
-    <div className="container">
-      <h3>Simple State and Lifecycle Management</h3>
+    const [text1, setText1] = useState("First");
 
-      <input onChange={(e) => setText1(e.target.value)} value={text1} />
-      <hr />
-      <input onChange={(e) => setText2(e.target.value)} value={text2} />
-      <hr />
-      <h2>
-        <i>
-          {text1} {text2}
-        </i>
-      </h2>
-    </div>
-  );
+    useEffect(() => {
+        document.title = `${text1.length}`;
+        console.log(text1.length);
+    })
+
+    const [text2, setText2] = useState("Last");
+
+    return (
+        <div className="container">
+            <h3>demo</h3>
+
+            <input onChange={(e) => setText1(e.target.value)} value={text1} />
+            <hr />
+
+            <input onChange={(e) => setText2(e.target.value)} value={text2} />
+            <hr />
+
+            <h2>
+                <i>{text1} {text2}</i>
+            </h2>
+        </div>
+    )
 }
